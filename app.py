909cbe07fb139da486c394c4392ed64e7e2104b1
Liu Min <lmibrain@gmail.com>
2016-12-08 22:27:23 +0800

    add flask

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+import os
+from infer_cnn_ocr import inference_init, inference_process
+
+app = Flask(__name__)
+UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__))
+app.config['UPLOAD_FOLDER'] = os.path.join(UPLOAD_FOLDER, 'static')
+
+
+inference_handle = inference_init()
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file(filename="2989.jpg", result="64862"):
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            result = inference_process(inference_handle, os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return render_template('index.html', filename=filename, result=result)
+    return render_template('index.html', filename=filename, result=result)
+
+if __name__ == '__main__':
+   app.run()
